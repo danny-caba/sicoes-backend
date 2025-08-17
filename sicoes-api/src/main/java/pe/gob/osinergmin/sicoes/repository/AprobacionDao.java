c1819e97b0a53a752ac2aea3dc62dfd85a3410fc
Patricio López <patricio.lopez@dlwlatam.com>
2025-08-17 11:26:57 -0500

    add asignacion gerente linea

--- sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/repository/AprobacionDao.java
+++ sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/repository/AprobacionDao.java
@@ -3,9 +3,14 @@ package pe.gob.osinergmin.sicoes.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.gob.osinergmin.sicoes.model.Aprobacion;
+import pe.gob.osinergmin.sicoes.model.PersonalReemplazo;
+
+import java.util.Optional;
 
 @Repository
 public interface AprobacionDao extends JpaRepository<Aprobacion, Long> {
+
+    Optional<Aprobacion> findByRemplazoPersonal(PersonalReemplazo personalReemplazo);
 
 
 }
