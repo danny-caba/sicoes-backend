e8e65e5671ab3df3f9c59db8b5a0e4d456bf38bc
YuriEcv <e.condori.v@gmail.com>
2025-08-22 20:15:36 -0500

    update list finforme

--- sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/repository/renovacioncontrato/SolicitudPerfecionamientoContratoDao.java
+++ sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/repository/renovacioncontrato/SolicitudPerfecionamientoContratoDao.java
@@ -24,8 +24,45 @@ public interface SolicitudPerfecionamientoContratoDao extends JpaRepository<Soli
     "LEFT JOIN SICOES_TR_PROCESO_ITEM procesoItem ON propuesta.ID_PROCESO_ITEM = procesoItem.ID_PROCESO_ITEM " +
     "LEFT JOIN SICOES_TR_PROCESO_ITEM_PERFIL procesoItemPerfil ON procesoItem.ID_PROCESO_ITEM = procesoItemPerfil.ID_PROCESO_ITEM " +
     "LEFT JOIN SICOES_TX_PERFIL_APROBADOR perfilAprobador ON procesoItemPerfil.ID_PERFIL_LD = perfilAprobador.ID_PERFIL " +
-    "WHERE spc.ID_SOLI_PERF_CONT = :idSolicitudPerfecionamientoContrato " ,
+    "WHERE spc.ID_SOLI_PERF_CONT = :idSolicitudPerfecionamientoContrato AND spc.DE_SOLICITUD = 'CONCLUIDO' " ,
     nativeQuery = true)
     List<SolicitudPerfecionamientoContrato> getPerfilAprobadorByIdPerfilListadoDetalle(
         @Param("idSolicitudPerfecionamientoContrato") Long idSolicitudPerfecionamientoContrato);
+   
+
+
+    @Query(value = "SELECT " +
+    "spc.ID_SOLI_PERF_CONT as ID_SOLI_PERF_CONT, " +
+    "procesoItemPerfil.ID_PERFIL_LD AS ID_PERFIL, " +
+    "perfilAprobador.ID_APROBADOR_G1 AS ID_APROBADOR_G1, " +
+    "perfilAprobador.ID_APROBADOR_G2 AS ID_APROBADOR_G2, " +
+    "perfilAprobador.ID_APROBADOR_G3 AS ID_APROBADOR_G3 " +
+    "FROM SICOES_TC_SOLI_PERF_CONT spc " +
+    "LEFT JOIN SICOES_TR_PROPUESTA propuesta ON spc.ID_PROPUESTA = propuesta.ID_PROPUESTA " +
+    "LEFT JOIN SICOES_TR_PROCESO_ITEM procesoItem ON propuesta.ID_PROCESO_ITEM = procesoItem.ID_PROCESO_ITEM " +
+    "LEFT JOIN SICOES_TR_PROCESO_ITEM_PERFIL procesoItemPerfil ON procesoItem.ID_PROCESO_ITEM = procesoItemPerfil.ID_PROCESO_ITEM " +
+    "LEFT JOIN SICOES_TX_PERFIL_APROBADOR perfilAprobador ON procesoItemPerfil.ID_PERFIL_LD = perfilAprobador.ID_PERFIL " +
+    "WHERE perfilAprobador.ID_APROBADOR_G1 = idUsuario AND spc.DE_SOLICITUD = 'CONCLUIDO' " ,
+    nativeQuery = true)
+    List<SolicitudPerfecionamientoContrato> getPerfilAprobadorByIdUsuarioG1(
+        @Param("idUsuario") Long idUsuario);
+
+
+
+    @Query(value = "SELECT " +
+    "spc.ID_SOLI_PERF_CONT as ID_SOLI_PERF_CONT, " +
+    "procesoItemPerfil.ID_PERFIL_LD AS ID_PERFIL, " +
+    "perfilAprobador.ID_APROBADOR_G1 AS ID_APROBADOR_G1, " +
+    "perfilAprobador.ID_APROBADOR_G2 AS ID_APROBADOR_G2, " +
+    "perfilAprobador.ID_APROBADOR_G3 AS ID_APROBADOR_G3 " +
+    "FROM SICOES_TC_SOLI_PERF_CONT spc " +
+    "LEFT JOIN SICOES_TR_PROPUESTA propuesta ON spc.ID_PROPUESTA = propuesta.ID_PROPUESTA " +
+    "LEFT JOIN SICOES_TR_PROCESO_ITEM procesoItem ON propuesta.ID_PROCESO_ITEM = procesoItem.ID_PROCESO_ITEM " +
+    "LEFT JOIN SICOES_TR_PROCESO_ITEM_PERFIL procesoItemPerfil ON procesoItem.ID_PROCESO_ITEM = procesoItemPerfil.ID_PROCESO_ITEM " +
+    "LEFT JOIN SICOES_TX_PERFIL_APROBADOR perfilAprobador ON procesoItemPerfil.ID_PERFIL_LD = perfilAprobador.ID_PERFIL " +
+    "WHERE perfilAprobador.ID_APROBADOR_G2 = idUsuario AND spc.DE_SOLICITUD = 'CONCLUIDO' " ,
+    nativeQuery = true)
+    List<SolicitudPerfecionamientoContrato> getPerfilAprobadorByIdUsuarioG2(
+        @Param("idUsuario") Long idUsuario);
+    
 }
--- sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/service/renovacioncontrato/impl/ListarInformeRenovacionContratoImpl.java
+++ sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/service/renovacioncontrato/impl/ListarInformeRenovacionContratoImpl.java
@@ -3,8 +3,11 @@ package pe.gob.osinergmin.sicoes.service.renovacioncontrato.impl;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.stereotype.Service;
 
 import pe.gob.osinergmin.sicoes.model.ListadoDetalle;
@@ -12,27 +15,33 @@ import pe.gob.osinergmin.sicoes.model.Usuario;
 import pe.gob.osinergmin.sicoes.model.dto.renovacioncontrato.InformeRenovacionContratoDTO;
 import pe.gob.osinergmin.sicoes.model.renovacioncontrato.InformeRenovacionContrato;
 import pe.gob.osinergmin.sicoes.model.renovacioncontrato.RequerimientoAprobacion;
+import pe.gob.osinergmin.sicoes.model.renovacioncontrato.SolicitudPerfecionamientoContrato;
 import pe.gob.osinergmin.sicoes.repository.renovacioncontrato.InformeRenovacionContratoDao;
+import pe.gob.osinergmin.sicoes.repository.renovacioncontrato.SolicitudPerfecionamientoContratoDao;
 import pe.gob.osinergmin.sicoes.service.ListadoDetalleService;
-import pe.gob.osinergmin.sicoes.service.renovacioncontrato.PerfilAprobadorService;
 import pe.gob.osinergmin.sicoes.service.renovacioncontrato.mapper.InformeRenovacionContratoMapper;
 import pe.gob.osinergmin.sicoes.util.Constantes;
 import pe.gob.osinergmin.sicoes.util.Contexto;
+import pe.gob.osinergmin.sicoes.util.ValidacionException;
 
 @Service
 public class ListarInformeRenovacionContratoImpl {
-    private final PerfilAprobadorService perfilAprobadorService;
+
+    private final Logger logger = LogManager.getLogger(ListarInformeRenovacionContratoImpl.class);
+
+    private final SolicitudPerfecionamientoContratoDao solicitudPerfecionamientoContratoDao;
     private final ListadoDetalleService listadoDetalleService;
     private final InformeRenovacionContratoDao informeRenovacionContratoDao;
 
 
        public ListarInformeRenovacionContratoImpl(
-        PerfilAprobadorService perfilAprobadorService,
         ListadoDetalleService listadoDetalleService,
-        InformeRenovacionContratoDao informeRenovacionContratoDao) {
-           this.perfilAprobadorService = perfilAprobadorService;
+        InformeRenovacionContratoDao informeRenovacionContratoDao,
+        SolicitudPerfecionamientoContratoDao solicitudPerfecionamientoContratoDao) {
            this.listadoDetalleService = listadoDetalleService;
            this.informeRenovacionContratoDao = informeRenovacionContratoDao;
+           this.solicitudPerfecionamientoContratoDao = solicitudPerfecionamientoContratoDao;
+           
        }
 
     public Page<InformeRenovacionContratoDTO> ejecutar(
@@ -54,34 +63,51 @@ public class ListarInformeRenovacionContratoImpl {
                 "G1"
         );
 
-        if (estadoLd == null) {
-            throw  new RuntimeException("Estado 'renovacion contrato' no encontrado en listado detalle");
+        if (estadoLd == null || grupoG1 == null) {
+            throw new ValidacionException("Configuración de listado detalle no encontrada para estado o grupo");
         }
 
-    // Inicializar la query
-    Page<InformeRenovacionContrato> listInforme = informeRenovacionContratoDao.findByFiltrosWithJoins(
+        if (usuarioCtx.getRoles().stream().noneMatch(rol -> rol.getCodigo().equals(Constantes.ROLES.APROBADOR_TECNICO))){
+            throw new ValidacionException(Constantes.CODIGO_MENSAJE.USUARIO_SIN_PERMISO_RENOVACION_CONTRATO);
+        }
+
+        Page<InformeRenovacionContrato> listInforme = informeRenovacionContratoDao.findByFiltrosWithJoins(
             numeroExpediente,
             esVigente,
             estado,
             idContratista,
             pageable
-    );
+        ); 
+        
+        
+        List<SolicitudPerfecionamientoContrato> perfilesG1   = 
+         solicitudPerfecionamientoContratoDao.getPerfilAprobadorByIdUsuarioG1(usuarioCtx.getIdUsuario());
 
-    // Determinar qué filtro aplicar
-    if (usuarioCtx.getRoles().stream().anyMatch(rol -> rol.getCodigo().equals(Constantes.ROLES.APROBADOR_TECNICO))) {
 
-        if (perfilAprobadorService.esPerfilAprobadorG1(usuarioCtx.getIdUsuario())) {
+        if (!perfilesG1 .isEmpty()) {
             listInforme = filtrarAprobaciones(listInforme,
                     estadoLd.getIdListadoDetalle(),
                     grupoG1.getIdListadoDetalle());
-
-        } else if (perfilAprobadorService.esPerfilAprobadorG2(usuarioCtx.getIdUsuario())) {
-            listInforme = filtrarAprobaciones(listInforme, 959L, 955L);
+        
+            return listInforme.map(InformeRenovacionContratoMapper.MAPPER::toDTO);
+        }else{
+            this.logger.warn("Usuario {} sin permiso G1 ",usuarioCtx.getIdUsuario());
         }
-    }
+        
+        
+        List<SolicitudPerfecionamientoContrato> perfilesG2   = 
+         solicitudPerfecionamientoContratoDao.getPerfilAprobadorByIdUsuarioG2(usuarioCtx.getIdUsuario());
+        
+        
+        if (!perfilesG2 .isEmpty()) {
+            listInforme = filtrarAprobaciones(listInforme, 959L, 955L);
 
-        return listInforme.map(InformeRenovacionContratoMapper.MAPPER::toDTO);
-    }
+            return listInforme.map(InformeRenovacionContratoMapper.MAPPER::toDTO);
+        }else {
+            this.logger.info("Usuario {} sin permiso G2 ",usuarioCtx.getIdUsuario());
+        }
+        throw new ValidacionException(Constantes.CODIGO_MENSAJE.USUARIO_SIN_PERMISO_RENOVACION_CONTRATO);
+}
 
     
     private Page<InformeRenovacionContrato> filtrarAprobaciones(Page<InformeRenovacionContrato> informes,
