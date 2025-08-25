0119dc2f01adedd876ba9cf1beb24015a35ce40c
Patricio López <patricio.lopez@dlwlatam.com>
2025-08-25 17:40:21 -0500

    fix buscarAprobacion

--- sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/repository/AprobacionDao.java
+++ sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/repository/AprobacionDao.java
@@ -2,6 +2,7 @@ package pe.gob.osinergmin.sicoes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.gob.osinergmin.sicoes.model.Aprobacion;
 
@@ -10,12 +11,15 @@ import java.util.Optional;
 @Repository
 public interface AprobacionDao extends JpaRepository<Aprobacion, Long> {
 
-    @Query( "select ap from Aprobacion ap "
-            + "left join fetch ap.remplazoPersonal rp "
-            + "where rp.idReemplazo=:idReemplazo "
-            + "and rownum <=1 "
-            + "order by ap.idAprobacion desc")
-    Optional<Aprobacion> findByRemplazoPersonal(Long idReemplazo);
+    @Query(value = "SELECT * FROM ("
+            + "  SELECT ap.* "
+            + "  FROM SICOES_TZ_APROBACION_REEMP ap "
+            + "  LEFT JOIN SICOES_TZ_REEMPLAZO_PERSONAL rp ON ap.ID_REEMPLAZO_PERSONAL = rp.ID_REEMPLAZO_PERSONAL "
+            + "  WHERE rp.ID_REEMPLAZO_PERSONAL = :idReemplazo "
+            + "  ORDER BY ap.FE_CREACION DESC"
+            + ") WHERE ROWNUM = 1",
+            nativeQuery = true)
+    Optional<Aprobacion> findByRemplazoPersonal(@Param("idReemplazo") Long idReemplazo);
 
 
 }
