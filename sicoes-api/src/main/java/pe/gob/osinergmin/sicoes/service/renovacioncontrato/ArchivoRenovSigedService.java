c6af2d54114b60b7b5326c6a09c28da9a96a0706
Wilber Mamani <wilbermamanisalguero@gmail.com>
2025-09-01 09:17:11 -0500

    test

---
+++ sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/service/renovacioncontrato/ArchivoRenovSigedService.java
@@ -0,0 +1,16 @@
+package pe.gob.osinergmin.sicoes.service.renovacioncontrato;
+
+import java.io.File;
+import java.util.List;
+
+import gob.osinergmin.siged.remote.rest.ro.in.DocumentoAnularInRO;
+import gob.osinergmin.siged.remote.rest.ro.in.ExpedienteInRO;
+import gob.osinergmin.siged.remote.rest.ro.out.DocumentoAnularOutRO;
+import gob.osinergmin.siged.remote.rest.ro.out.DocumentoOutRO;
+
+public interface ArchivoRenovSigedService {
+
+    DocumentoOutRO agregarDocumento(ExpedienteInRO expediente, List<File> archivos) throws Exception;
+    
+    DocumentoAnularOutRO anularDocumento(DocumentoAnularInRO documento) throws Exception;
+}
