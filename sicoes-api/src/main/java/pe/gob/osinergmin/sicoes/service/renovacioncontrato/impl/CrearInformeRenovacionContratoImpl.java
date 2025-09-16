20cb829dfa0f6da5b515bcd3d6b18cda0654a95d
Wilber Mamani <wilbermamanisalguero@gmail.com>
2025-09-15 19:16:11 -0500

    test

--- sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/service/renovacioncontrato/impl/CrearInformeRenovacionContratoImpl.java
+++ sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/service/renovacioncontrato/impl/CrearInformeRenovacionContratoImpl.java
@@ -242,7 +242,20 @@ public class CrearInformeRenovacionContratoImpl  {
 
             adjuntarDocumentoSiged(informe, archivoPdf.getNombreReal(), bytesSalida, solicitud);
 
+            ListadoDetalle EnAprobacionEstadoAprobacionInforme = listadoDetalleService.obtenerListadoDetalle(
+                    "ESTADO_REQUERIMIENTO",
+                    "EN_APROBACION"
+            );
+
+            informe.setEstadoAprobacionInforme(EnAprobacionEstadoAprobacionInforme);
             nuevoInformeRenovacionContrato = informeRenovacionContratoDao.save(informe);
+
+            ListadoDetalle EnProcesoEstadoRequerimientoRenovacion = listadoDetalleService.obtenerListadoDetalle(
+                    "ESTADO_REQUERIMIENTO",
+                    "EN_PROCESO"
+            );
+            requerimientoRenovacion.setEstadoReqRenovacion(EnProcesoEstadoRequerimientoRenovacion);
+            requerimientoRenovacionDao.save(requerimientoRenovacion);
 
             archivoPdf.setIdInformeRenovacion(nuevoInformeRenovacionContrato.getIdInformeRenovacion());
             archivoPdf = archivoDao.save(archivoPdf);
