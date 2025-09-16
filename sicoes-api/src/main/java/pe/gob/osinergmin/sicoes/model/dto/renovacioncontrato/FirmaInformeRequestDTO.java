ece4c758a236be27eb3ad5d5a394bedff8c94603
Wilber Grover Mamani Salguero <wilber.mamani@dlwlatam.com>
2025-09-15 22:13:50 -0500

    TEST

--- sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/model/dto/renovacioncontrato/FirmaInformeRequestDTO.java
+++ sicoes-api/src/main/java/pe/gob/osinergmin/sicoes/model/dto/renovacioncontrato/FirmaInformeRequestDTO.java
@@ -11,6 +11,6 @@ public class FirmaInformeRequestDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Long idRequerimientoAprobacion;
-    private Long idInformeRenovacion;
+
 
 }
